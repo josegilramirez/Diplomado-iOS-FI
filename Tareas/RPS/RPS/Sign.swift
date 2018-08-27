e821861d83b6470b3fd232ad6c84cefb21b2b9dd
Jose Gil Ramirez Sanchez <josegil_ramirez@sps.com.mx>
2018-08-27 11:54:05 -0500

    prueba de actualización

--- Tareas/RPS/RPS/Sign.swift
+++ Tareas/RPS/RPS/Sign.swift
@@ -9,21 +9,6 @@
 import Foundation
 import GameplayKit
 
-let randomChoice = GKRandomDistribution(lowestValue: 0, highestValue: 2)
-
-func randomSign() -> Sign {
-    let sign = randomChoice.nextInt()
-    if sign == 0 {
-        return .rock
-    }
-    else if sign == 1 {
-        return .paper
-    }
-    else {
-        return .scissors
-    }
-}
-
 enum Sign {
     case rock, paper, scissors
     var emoji: String {
@@ -71,3 +56,19 @@ enum Sign {
         }
     }
 }
+
+let randomChoice = GKRandomDistribution(lowestValue: 0, highestValue: 2)
+
+func randomSign() -> Sign {
+    let sign = randomChoice.nextInt()
+    if sign == 0 {
+        return .rock
+    }
+    else if sign == 1 {
+        return .paper
+    }
+    else {
+        return .scissors
+    }
+}
+
