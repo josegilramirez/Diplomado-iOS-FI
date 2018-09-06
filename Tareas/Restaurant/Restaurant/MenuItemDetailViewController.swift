b4d777e116209fe1bdc9cecc124f92692aaf8e1e
Jose Gil Ramirez Sanchez <josegil_ramirez@sps.com.mx>
2018-09-06 00:16:41 -0500

    Part 1

---
+++ Tareas/Restaurant/Restaurant/MenuItemDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MenuItemDetailViewController.swift
+//  Restaurant
+//
+//  Created by José Gil Ramírez S on 06/09/18.
+//  Copyright © 2018 José Gil Ramírez S. All rights reserved.
+//
+
+import UIKit
+
+class MenuItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Tareas/Restaurant/Restaurant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by José Gil Ramírez S on 05/09/18.
+//  Copyright © 2018 José Gil Ramírez S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
