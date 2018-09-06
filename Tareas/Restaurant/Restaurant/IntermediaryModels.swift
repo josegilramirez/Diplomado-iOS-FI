acbe675a03203b32a3cb3c60b018be28b93843f9
Jose Gil Ramirez Sanchez <josegil_ramirez@sps.com.mx>
2018-09-06 11:16:43 -0500

    Part V

---
+++ Tareas/Restaurant/Restaurant/IntermediaryModels.swift
@@ -0,0 +1,20 @@
+//
+//  IntermediaryModels.swift
+//  Restaurant
+//
+//  Created by José Gil Ramírez S on 06/09/18.
+//  Copyright © 2018 José Gil Ramírez S. All rights reserved.
+//
+
+import Foundation
+
+struct Categories: Codable {
+    let categories: [String]
+}
+
+struct PreparationTime: Codable {
+    let preparationTime: Int
+    enum CodingKeys: String, CodingKey {
+        case preparationTime = "preparation_time"
+    }
+}
---
+++ Tareas/Restaurant/Restaurant/MenuItem.swift
@@ -0,0 +1,31 @@
+//
+//  MenuItem.swift
+//  Restaurant
+//
+//  Created by José Gil Ramírez S on 06/09/18.
+//  Copyright © 2018 José Gil Ramírez S. All rights reserved.
+//
+
+import Foundation
+
+struct MenuItem: Codable {
+    var id: Int
+    var name: String
+    var description: String
+    var price: Double
+    var category: String
+    var imageURL: URL
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case description
+        case price
+        case category
+        case imageURL = "image_url"
+    }
+}
+
+struct MenuItems: Codable {
+    let items: [MenuItem]
+}
